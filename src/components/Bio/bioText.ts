2d93b05d8f34c1c43ec62bc7e5679409ded33fff
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2022-07-10 16:31:51 -0700

    updated text margins and bioTExt

--- src/components/Bio/bioText.ts
+++ src/components/Bio/bioText.ts
@@ -1,10 +1,10 @@
 const bioText = `
-  Hello, my name is Bryan Alvarez. I am a full stack web developer. 
-  I specialize on the React Framework, but also have experience with the Vue Framework and traditional (JS, CSS, HTML).  
-  I attended college for computer information systems and computer science. 
-  I am continuously working to expand my expertise in the ever-changing world of web development. 
-  I also have a strong background in customer service, data analysis, and system administration. 
-  I am a self-starter who enjoys facing challenging problems head on and the satisfaction that comes with finding a solution.
-`
+  My name is Bryan Alvarez, I am a full stack software engineer. 
+  I work as a full time web developer and do some freelancing on the side.  
+  My motivation comes from two places: desire to continuously expand my expertise in the ever-changing field of software development; 
+  satisfaction that comes from finding a solution to challenging problems. 
+  Along the way I have picked up valuable complimentary skills in customer service, employee relations, leadership, teamwork, data analysis, and system administration. 
+  I will leave you that, thank you for your time. 
+  `
 
 export default bioText
