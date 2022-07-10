17d3498933d86585a757d54fbe98818e3a92eeaa
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2022-07-10 15:00:33 -0700

    added key

--- src/components/TechLogo/logos.tsx
+++ src/components/TechLogo/logos.tsx
@@ -2,13 +2,15 @@ import TechLogo from './TechLogo'
 import Logos from './logoMap'
 
 export default Object.keys(Logos).map( key => 
-  <TechLogo 
-    id={Logos[key].id}
-    href={Logos[key].href}
-    image={Logos[key].image}
-    alt={Logos[key].alt}
-    title={Logos[key].title}
-  />
+  <div key={Logos[key].id}>
+    <TechLogo 
+      id={Logos[key].id}
+      href={Logos[key].href}
+      image={Logos[key].image}
+      alt={Logos[key].alt}
+      title={Logos[key].title}
+    />
+  </div>
 )
 
  
