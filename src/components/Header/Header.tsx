629f492618eadc8cfa1ff3f13c4396aa1e80c3ae
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2022-07-10 14:49:49 -0700

    removed window from initial load

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,35 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import BurgerMenu from '../BurgerMenu/Burger'
-
 import linkedIn from '../../../public/assets/linkedin.logo.svg'
 import gitHub from '../../../public/assets/github.logo.svg'
 
 import styles from './header.module.css'
 
 const Header = () => {
-  if (typeof window !== "undefined") {
-    const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
-    
-    const handleScroll = useCallback(
-      () => {
-        const currentScrollPos = window.pageYOffset;
-        const topOfPage = window.pageYOffset === 0;
-        const bottomOfPage = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
-        const userScrolledUp = prevScrollpos > currentScrollPos && currentScrollPos !== 0;
-        setPrevScrollpos(currentScrollPos);
-        
-        if ( !userScrolledUp || bottomOfPage ) setVisible(false);
-        if ( userScrolledUp || topOfPage) setVisible(true);
-        
-      },[prevScrollpos],
-      )
-      
-      useEffect(() => {
-        window.addEventListener("scroll", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-    }, [handleScroll])
-  }
-
+  const [prevScrollpos, setPrevScrollpos] = useState(0);
   const [visible, setVisible] = useState(true)
+  
+  const handleScroll = useCallback(
+    () => {
+      const currentScrollPos = window.pageYOffset;
+      const topOfPage = window.pageYOffset === 0;
+      const bottomOfPage = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+      const userScrolledUp = prevScrollpos > currentScrollPos && currentScrollPos !== 0;
+      setPrevScrollpos(currentScrollPos);
+      
+      if ( !userScrolledUp || bottomOfPage ) setVisible(false);
+      if ( userScrolledUp || topOfPage) setVisible(true);
+      
+    },[prevScrollpos],
+    )
+    
+    useEffect(() => {
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll])
+  
+  
   const navBarClassName = !visible ? `${styles.header} ${styles.hidden}` : styles.header
-
   return (
     <nav className={navBarClassName}> 
      <BurgerMenu />
