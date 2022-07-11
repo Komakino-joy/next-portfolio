7f38f4f407ca6964e209d9b6b36dd886bd4c4de1
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2022-07-10 20:25:39 -0700

    updated bio

--- src/components/Bio/bioText.ts
+++ src/components/Bio/bioText.ts
@@ -3,8 +3,7 @@ const bioText = `
   I work as a full time web developer and do some freelancing on the side.  
   My motivation comes from two places: desire to continuously expand my expertise in the ever-changing field of software development; 
   satisfaction that comes from finding a solution to challenging problems. 
-  Along the way I have picked up valuable complimentary skills in customer service, employee relations, leadership, teamwork, data analysis, and system administration. 
-  I will leave you that, thank you for your time. 
+  Along the way I have picked up valuable complimentary skills in customer service, employee relations, leadership, teamwork, data analysis, and system administration.
   `
 
 export default bioText
