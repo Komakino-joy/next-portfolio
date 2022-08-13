f809ca062049c1979a5ec92162df7c12d9bf262a
Bryan Alvarez <bryan.alvarez1251@gmail.com>
2022-08-12 21:05:35 -0700

    bio update

--- src/components/Bio/bioText.ts
+++ src/components/Bio/bioText.ts
@@ -1,8 +1,7 @@
 const bioText = `
-  My name is Bryan Alvarez, I am a full stack software engineer. 
-  I work as a full time web developer and do some freelancing on the side.  
-  My motivation comes from two places: desire to continuously expand my expertise in the ever-changing field of software development; 
-  satisfaction that comes from finding a solution to challenging problems. 
+  My name is Bryan Alvarez, I am a full time software engineer and do some freelancing on the side.  
+  My motivation comes from two places: desire to continuously expand my expertise in the ever-changing field of software engineering; 
+  the satisfaction that comes with finding a solution to challenging problems. 
   Along the way I have picked up valuable complimentary skills in customer service, employee relations, leadership, teamwork, data analysis, and system administration.
   `
 
